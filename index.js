9ddb6ddfd9a587e58c54886c87a84a636105af62
dhruvapanyam <dhruva.panyam_ug21@ashoka.edu.in>
2021-03-17 11:31:02 +0530

    --

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ var server = app.listen(port,function(){
 })
 
 
-// app.use(express.static('front_end'));
+app.use(express.static('front_end'));
 
 
-if (process.env.NODE_ENV === "production") {
+// if (process.env.NODE_ENV === "production") {
 
-    app.use(express.static("front_end"));
+//     app.use(express.static("front_end"));
 
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-    });
-}
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+//     });
+// }
